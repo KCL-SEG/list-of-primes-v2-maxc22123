cf483f69342beb17a0eba3c9845e39c98d7ecceb
Tsz Shun Max Chang <tsz.chang@kcl.ac.uk>
2022-10-01 15:38:36 +0100

    Complete prime numbers generator

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes < 1:
+        raise ValueError
+    else:
+        lst = [2]
+        p = 3
+        while len(lst) < number_of_primes:
+            for i in range(2,p):
+                if p % i == 0:
+                    p += 1
+                    break
+            else:
+                lst.append(p)
+                p += 1
+
+
+    return lst
